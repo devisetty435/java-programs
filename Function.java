55e0d94b20fc485d0113a540e3a8ed412d98e0e8
devisetty435 <ddevi7028@gmail.com>
2025-06-06 15:15:59 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,14 @@
+public class Function
+{
+    public static void add()
+    {
+    int a=5;
+    int b=5;
+    int c=a+b;
+    System.out.println(c);
+    }
+    public static void main(String[] args)
+    {
+        add();
+    }
+}
