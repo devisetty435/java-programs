0b6bf3f7c1b089c08a0c7bb081dd741f81666a89
devisetty435 <ddevi7028@gmail.com>
2025-06-10 14:39:07 +0530

    Add files via upload

---
+++ bubble sort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int []a=new int[5];
+		Scanner sc=new Scanner(System.in);
+		for(int i=0;i<a.length;i++)
+		{
+		    a[i]=sc.nextInt();
+		}
+		for(int i=0;i<a.length-1;i++)
+		{
+		    for(int j=0;j<a.length-1-i;j++)
+		    {
+		        if(a[j]>a[j+1])
+		        {
+		            int temp=a[j];
+		            a[j]=a[j+1];
+		            a[j+1]=temp;
+		        }
+		    }
+		}
+		for (int i=0;i<a.length;i++)
+		{
+		    System.out.println(a[i]);
+		}
+		
+	}
+}
