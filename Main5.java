f851997ffedd1d507e291aa515de51727bda58ef
devisetty435 <ddevi7028@gmail.com>
2025-06-17 14:05:15 +0530

    Add files via upload

---
+++ Main5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main5{
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.println(a.toLowerCase());
+        System.out.println(a.toUpperCase());
+        
+        }
+}
+
